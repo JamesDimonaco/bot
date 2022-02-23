11dd996ce2c084b84ff06bbe3861b144c3f1a4db
jamie-legg <legg.jamie@gmail.com>
2022-02-23 13:25:04 +0000

    env added

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,20 @@
 import { Client } from "discord.js";
 
 console.log("Bot is starting...");
+require('dotenv').config();
 
 const client = new Client({
   intents: [],
 });
 
-console.log(client);
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user!.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
